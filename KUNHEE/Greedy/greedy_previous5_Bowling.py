7bdb6575b57f01cb1f42a52e8b77cccd0a73a7a9
hkh9715 <68491809+hkh9715@users.noreply.github.com>
2021-12-03 19:28:34 +0900

    add footnote & memo

--- KUNHEE/Greedy/greedy_previous5_Bowling.py
+++ KUNHEE/Greedy/greedy_previous5_Bowling.py
@@ -3,6 +3,7 @@
 # weights = list(map(int, input().split()))
 
 # cnt = 0
+
 # for a in weights:
 #     for b in weights:
 #         if a == b:
@@ -10,6 +11,8 @@
 #         else :
 #             cnt += 1
 
+## 나누기 2 하는 이유는 순서와 관계없으므로 중복 제거
+# 예를 들어, 위 문제에서 (1, 2)와 (2, 1)는 같은 순서쌍.
 # real_cnt = int(cnt / 2)
 # print(real_cnt)
 
--- KUNHEE/Greedy/greedy_previous5_Bowling_sol.py
+++ KUNHEE/Greedy/greedy_previous5_Bowling_sol.py
@@ -1,10 +1,14 @@
 n, m = map(int, input().split())
 data = list(map(int, input().split()))
 
+# 원소가 0이고 길이가 11인 리스트 생성하고 볼링공의 개수 정리
+# 파이썬은 0 부터 시작하니까 원소가 11개.
 array = [0] * 11
 for x in data:
     array[x] += 1
 
+# 1부터 순서쌍을 구하고 원소개수 곱해주기
+# 그러고 나서 1 삭제하고 2부터 반복하기
 result = 0
 for i in range(1, m + 1):
     n -= array[i]
