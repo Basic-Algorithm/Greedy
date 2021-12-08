d12b48b6da2ddaa6a1003a80565b9a32efb8064f
Icefin <wjrchorh135@gmail.com>
2021-12-09 00:06:52 +0900

    finished code so far

--- GeonHo/Sorting/3_Change_Elements.cpp
+++ GeonHo/Sorting/3_Change_Elements.cpp
@@ -15,7 +15,6 @@ void Initialize();
 int main()
 {
 	Initialize();
-
 	//A는 오름차순 정렬
 	sort(arr_A.begin(), arr_A.end());
 	//B는 내림차순 정렬
@@ -31,23 +30,4 @@ int main()
 	//accumulate : arr_A.begin()~ end()까지 원소의 합, 초기값은 0
 	//numeric 헤더 필요
 	cout << accumulate(arr_A.begin(), arr_A.end(), 0);
-}
-
-void Initialize()
-{
-	cin >> N >> K;
-
-	for (int i = 0; i < N; i++)
-	{
-		int x;
-		cin >> x;
-		arr_A.push_back(x);
-	}
-
-	for (int i = 0; i < N; i++)
-	{
-		int x;
-		cin >> x;
-		arr_B.push_back(x);
-	}
 }
